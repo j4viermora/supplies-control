29f3a2d37149bd0c2d770dba9a1cf41790df343c
Javier A Mora Gil <j4viermora@gmail.com>
2023-02-05 19:37:04 -0400

    feat: :sparkles: add modes, enable development only style and layour withour internet

--- frontend/src/composables/useSession.ts
+++ frontend/src/composables/useSession.ts
@@ -1,24 +1,46 @@
 import { ref, reactive } from "vue";
 import * as user from "../api/user";
+import { ISession } from "../interfaces/session.interfaces";
+
+const isOffline = import.meta.env.VITE_MODE === "offline";
+
+console.log(isOffline);
 
 export const useSession = () => {
-  const session = reactive({
+  const session = reactive<ISession>({
     user: {
-      id: "1",
       name: "javier",
+      email: "j4viermora@gmail.com",
+      _id: "",
+      lastName: "",
+      roles: [""],
     },
     company: {
+      _id: "",
+      code: "",
+      createdAt: "",
+      owner: "",
+      status: "",
+      updatedAt: "",
       name: "compnay1",
-      id: "1",
+      __v: 0,
     },
   });
   const isLoading = ref(true);
 
   const getSession = async () => {
     isLoading.value = true;
-    const { data } = await user.getSession();
-    console.log(data);
-    isLoading.value = false;
+    try {
+      if (isOffline) {
+        return;
+      }
+      const { data } = await user.getSession();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      isLoading.value = false;
+    }
   };
 
   return {
